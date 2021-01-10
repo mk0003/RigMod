09c466296e66d6834119985e4a31ca83dd1f20df
Mitat Koyuncu <mitat_94_@hotmail.com>
2021-01-10 19:19:30 +0300

    Blacklist/Whitelist configs added.

--- src/main/java/com/mitat/rigmod/block/RandomItemGeneratorBlock.java
+++ src/main/java/com/mitat/rigmod/block/RandomItemGeneratorBlock.java
@@ -19,7 +19,7 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.Collection;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Optional;
 
@@ -58,20 +58,50 @@ public class RandomItemGeneratorBlock extends Block {
 				fortuneLevel = 0;
 
 			if (!hasSilkTouch) {
-				Collection<ResourceLocation> itemKeyList = ForgeRegistries.ITEMS.getKeys();
+				ArrayList<Item> itemList = new ArrayList<>();
+				for (int i = 0; i <= ForgeRegistries.ITEMS.getKeys().size(); i++) {
+					Optional<ResourceLocation> itemKey = ForgeRegistries.ITEMS.getKeys().stream().skip(i).findFirst();
+					if (itemKey.isPresent()) {
+						Item item = ForgeRegistries.ITEMS.getValue(itemKey.get());
+						if (item != null) {
+							ResourceLocation itemRegistry = item.getRegistryName();
+							if (itemRegistry != null) {
+								boolean isItemWhitelisted = Config.ITEM_WHITELIST.get().contains(itemRegistry.toString());
+								boolean isItemBlacklisted = Config.ITEM_BLACKLIST.get().contains(itemRegistry.toString());
+								boolean isModWhitelisted = Config.MOD_WHITELIST.get().contains(itemRegistry.getNamespace());
+								boolean isModBlacklisted = Config.MOD_BLACKLIST.get().contains(itemRegistry.getNamespace());
+								boolean isTagWhitelisted = false;
+								for (ResourceLocation tag : item.getTags()) {
+									isTagWhitelisted = Config.TAG_WHITELIST.get().contains(tag.toString());
+									if (isTagWhitelisted)
+										break;
+								}
+								boolean isTagBlacklisted = false;
+								for (ResourceLocation tag : item.getTags()) {
+									isTagBlacklisted = Config.TAG_BLACKLIST.get().contains(tag.toString());
+									if (isTagBlacklisted)
+										break;
+								}
+
+								if (!isItemBlacklisted && !isModBlacklisted && !isTagBlacklisted)
+									if (Config.ITEM_WHITELIST.get().isEmpty() || isItemWhitelisted)
+										if (Config.MOD_WHITELIST.get().isEmpty() || isModWhitelisted)
+											if (Config.TAG_WHITELIST.get().isEmpty() || isTagWhitelisted)
+												itemList.add(item);
+							}
+						}
+					}
+				}
 
 				int fortuneCount = calculateFortune(fortuneLevel);
 
 				for (int i = 0; i <= fortuneCount; i++) {
 
-					int randomIndex = (int) (Math.random() * itemKeyList.size());
-					Optional<ResourceLocation> key = itemKeyList.stream().skip(randomIndex).findFirst();
+					int randomIndex = (int) (Math.random() * itemList.size());
 
-					if (key.isPresent()) {
-						Item randomItem = ForgeRegistries.ITEMS.getValue(key.get());
-						if (randomItem != null)
-							InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(randomItem));
-					}
+					Item randomItem = itemList.get(randomIndex);
+					if (randomItem != null)
+						InventoryHelper.spawnItemStack(world, pos.getX(), pos.getY(), pos.getZ(), new ItemStack(randomItem));
 				}
 
 				return false;
--- src/main/java/com/mitat/rigmod/setup/Config.java
+++ src/main/java/com/mitat/rigmod/setup/Config.java
@@ -5,15 +5,27 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 
+import java.util.ArrayList;
+
 @Mod.EventBusSubscriber
 public class Config {
 
 	public static final String CATEGORY_GENERAL = "general";
+	public static final String CATEGORY_BLACKLIST_WHITELIST = "blacklist/whitelist";
 
 	public static ForgeConfigSpec SERVER_CONFIG;
 
 	public static ForgeConfigSpec.BooleanValue ALLOW_FORTUNE;
 	public static ForgeConfigSpec.BooleanValue ALLOW_SILK_TOUCH;
+
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> ITEM_WHITELIST;
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> ITEM_BLACKLIST;
+
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> MOD_WHITELIST;
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> MOD_BLACKLIST;
+
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> TAG_WHITELIST;
+	public static ForgeConfigSpec.ConfigValue<ArrayList<String>> TAG_BLACKLIST;
 
 	static {
 
@@ -23,6 +35,19 @@ public class Config {
 
 		ALLOW_FORTUNE = SERVER_BUILDER.comment("Allow fortune enchantment to give multiple drops").define("allowFortune", true);
 		ALLOW_SILK_TOUCH = SERVER_BUILDER.comment("Allow silk touch enchantment to drop item itself").define("allowSilkTouch", true);
+
+		SERVER_BUILDER.pop();
+
+		SERVER_BUILDER.comment("Blacklist/Whitelist settings").push(CATEGORY_BLACKLIST_WHITELIST);
+
+		ITEM_WHITELIST = SERVER_BUILDER.comment("Whitelisted Items (Leave empty to whitelist all items unless blacklist specified)").define("item_whitelist", new ArrayList<>());
+		ITEM_BLACKLIST = SERVER_BUILDER.comment("Blacklisted Items").define("item_blacklist", new ArrayList<>());
+
+		MOD_WHITELIST = SERVER_BUILDER.comment("Whitelisted Mods (Leave empty to whitelist all mods unless blacklist specified)").define("mod_whitelist", new ArrayList<>());
+		MOD_BLACKLIST = SERVER_BUILDER.comment("Blacklisted Mods").define("mod_blacklist", new ArrayList<>());
+
+		TAG_WHITELIST = SERVER_BUILDER.comment("Whitelisted Tags (Leave empty to whitelist all tags unless blacklist specified)").define("tag_whitelist", new ArrayList<>());
+		TAG_BLACKLIST = SERVER_BUILDER.comment("Blacklisted Tags").define("tag_blacklist", new ArrayList<>());
 
 		SERVER_BUILDER.pop();
 
